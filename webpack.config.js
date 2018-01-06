62bd433c96061d7c7ec10ad224d6278a74e4bfc7
Макс Недашковский <max0350@gmail.com>
2018-01-06 07:16:26 +0300

    add progect configuration

---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const env = process.env.NODE_ENV;
+let sourceMap;
+
+if (env === 'dev') {
+    sourceMap = true;
+} else if (env === 'production') {
+    sourceMap = false;
+}
+    
+
+module.exports = {
+    entry: {
+        // 'css/style.css': './src/scss/style.scss',
+        'js/index.min.js': './src/js/index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name]'
+        // publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ],
+                exclude: /(node_modules|bower_components)/
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            minimize: true,
+                            sourceMap: sourceMap
+                        }
+                    }, 
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: sourceMap
+                        }
+                    }, 
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: sourceMap
+                        }
+                    }]
+                })
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif)$/,
+                use: [
+                    {
+                      loader: 'file-loader',
+                      options: {
+                          name: '[name].[ext]',
+                          outputPath: 'img/'
+                      }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJsPlugin({ minimize: true, sourceMap: sourceMap }),
+        new ExtractTextPlugin({
+            filename:  'css/style.min.css',
+            allChunks: true
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    devtool: sourceMap ? 'source-map' : ''
+};
